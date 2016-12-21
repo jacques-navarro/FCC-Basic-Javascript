346c88ba2eccaa638d4ee931d235f46ab6affd1a
Jacques Navarro <jacques.navarro@gmail.com>
2016-12-20 20:53:13 -0500

    Using Objects for Lookups solution
      result = lookup[val];

--- Using Objects for Lookups.js
+++ Using Objects for Lookups.js
@@ -6,13 +6,14 @@ function phoneticLookup(val) {
 
   // Only change code below this line
   var lookup = {
-    "alpha": = "Adams";
-    "bravo": = "Boston";
-    "charlie": = "Chicago";
-    "delta": = "Denver";
-    "echo": = "Easy";
-    "foxtrot": = "Frank";
-  }
+    "alpha": "Adams",
+    "bravo": "Boston",
+    "charlie": "Chicago",
+    "delta": "Denver",
+    "echo": "Easy",
+    "foxtrot": "Frank",
+  };
+  result = lookup[val];
 
   // Only change code above this line
   return result;
